cdf0561fbee21183aa98c5e5866188f123077b8a
zenitugo <ugochiukaegbu19@gmail.com>
2025-04-14 18:25:31 +0100

    solution to task 1

--- Go-01/main.go
+++ Go-01/main.go
@@ -1,3 +1,5 @@
+First Go Program
+
 package main  // Declares the package name
 
 import "fmt"  // Imports the format package
@@ -5,3 +7,18 @@ import "fmt"  // Imports the format package
 func main() {  // Main function where execution begins
     fmt.Println("Hello, World!")
 }
+
+
+// Task 1
+
+package main  
+
+import "fmt" 
+
+func main() { 
+    var name string = "Ugochi"
+    var age int = 35 
+    fmt.Printf("Hello, %s! You are %d! years.\n", name, age)
+}
+
+
