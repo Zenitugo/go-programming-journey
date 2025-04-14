4387384d146cd2523a2cbf3c4a2b5a6c7176829d
zenitugo <ugochiukaegbu19@gmail.com>
2025-04-14 17:53:37 +0100

    my first go program

---
+++ Go-01/main.go
@@ -0,0 +1,7 @@
+package main  // Declares the package name
+
+import "fmt"  // Imports the format package
+
+func main() {  // Main function where execution begins
+    fmt.Println("Hello, World!")
+}
