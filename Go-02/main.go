8744ef4af871f0b1cb0323a049341b5bd98054e4
zenitugo <ugochiukaegbu19@gmail.com>
2025-05-08 19:08:15 +0100

    groks solution to mini project 1

--- Go-02/main.go
+++ Go-02/main.go
@@ -1,3 +1,4 @@
+// Mini Project 1
 package main
 
 import "fmt"
@@ -15,3 +16,28 @@ func main() {
 	fmt.Printf("Sum: %d\n", sum)
 }
 
+// Grok's Solution to Mini Project 1	
+package main
+
+import (
+	"fmt"
+)
+func sumNumbers(num int) int {
+	total := 0
+	for i := 1; i <= num; i++ {
+		total += i
+	}
+	return total
+}
+
+func main() {
+	var num int
+	fmt.Print("Enter a positive number: ")
+	_, err := fmt.Scanln(&num)
+	if err != nil || num <= 0 {
+		fmt.Println("Invalid input: Enter a positive number")
+		return
+	}
+	result := sumNumbers(num)
+	fmt.Printf("Sum: %d\n", result)
+}
